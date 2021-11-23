0df46edd0fcffd7c86fc8c57bae20eabd68c8d33
derrickkarake <african.derrick@gmial.com>
2021-11-22 22:57:58 -0600

    changes to speech.txt and added colors.txt

--- try.py
+++ try.py
@@ -4,7 +4,7 @@ fout = open("out.txt", "wt")
 #for each line in the input file
 for line in fin:
 	#read replace the string and write to output file
-	fout.write(line.replace('the', 'pickle'))
+	fout.write(line.replace('and', 'not'))
 #close input and output files
 fin.close()
 fout.close()
